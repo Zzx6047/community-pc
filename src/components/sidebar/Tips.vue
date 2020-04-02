bd7fe2572be85736b50f985f43639db6294cea6f
zhaojinxin <474969612@qq.com>
2020-04-02 17:46:40 +0800

    pc端第一次提交

---
+++ src/components/sidebar/Tips.vue
@@ -0,0 +1,60 @@
+<!--  -->
+<template>
+  <div class="fly-panel">
+    <h3 class="fly-panel-title">温馨通道</h3>
+    <div class="fly-panel-main layui-row">
+      <ul class="layui-clear imooc-quick">
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">layui 的 GitHub 及 Gitee (码云) 仓库，欢迎Star</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">layui 的 GitHub 及 Gitee (码云) 仓库，欢迎Star</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">layui 的 GitHub 及 Gitee (码云) 仓库，欢迎Star</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">123</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tips',
+  data () {
+    return {}
+  },
+  components: {},
+  computed: {},
+  mounted: {},
+  methods: {}
+}
+</script>
+<style lang='scss' scoped>
+$border-color: #f2f2f2;
+.fly-panel-main {
+  padding: 15px;
+}
+.imooc-quick {
+  border: 1px solid $border-color;
+  border-bottom: none;
+  border-right: none;
+  .layui-col-xs6 {
+    height: 40px;
+    line-height: 40px;
+    padding: 0 10px;
+    border: 1px solid $border-color;
+    text-align: center;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    border-left: none;
+    border-top: none;
+    a {
+      display: block;
+    }
+  }
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home.vue'
+const Login = () => import(/* webpackChunkName: 'login' */ './views/Login.vue')
+const Reg = () => import(/* webpackChunkName: 'reg' */ './views/Reg.vue')
+const Forget = () =>
+  import(/* webpackChunkName: 'forget' */ './views/Forget.vue')
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/reg',
+      name: 'reg',
+      component: Reg,
+      beforeEnter: (to, from, next) => {
+        if (from.name === 'login') {
+          next()
+        } else {
+          next('/login')
+        }
+      }
+    },
+    {
+      path: '/forget',
+      name: 'forget',
+      component: Forget
+    }
+  ]
+})
